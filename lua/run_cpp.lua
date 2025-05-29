6975e128550311fa32ca4fdf57d08a53a1f9b3e3
voldemort401 <181987124+voldemort401@users.noreply.github.com>
2025-05-29 10:21:46 +0545

    Adding files

---
+++ lua/run_cpp.lua
@@ -0,0 +1,33 @@
+-- RUN CPP FILES AUTOMATICALLY FROM NVIM ITSELF
+--
+--
+
+local M = {}
+
+function M.compile_file()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local wd = vim.fn.getcwd()
+  local Job = require 'plenary.job'
+
+  Job:new({
+    command = Compiler,
+    args    = { current_file, '' },
+    cwd     = wd,
+  }):sync() -- or start()
+end
+
+function M.setup(opts)
+  opts = opts or {}
+  vim.api.nvim_create_user_command("compile", M.run_file, {})
+
+  local keymap  = opts.keymap or '<leader>cm'
+  Compiler      = opts.compiler or 'g++'
+  Compiler_args = opts.compiler_args or '-o main.out'
+
+  vim.keymap.set('n', keymap, M.compile_file, {
+    desc = "run",
+    silent = true -- Prevents the command from being echoed in the command line
+  })
+end
+
+return M
